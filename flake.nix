5a8dac9364819a85fad3f67f53651f2bcc7743ad
metaporia <kyahnkrafft@me.com>
2024-06-02 19:48:12 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "tetris-rs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            rust-bin.beta.latest.default
+
+            # bevy deps
+            udev
+            wayland
+            alsaLib
+            libxkbcommon
+            vulkan-loader
+
+          ];
+
+          # drop into fish shell for development
+          shellHook = ''
+            fish
+          '';
+        };
+      }
+    );
+}
