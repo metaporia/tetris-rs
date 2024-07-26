d6c2ad10c8ccc91ffda482dbae8f959a89f2c68e
metaporia <kyahnkrafft@me.com>
2024-07-25 21:13:18 -0700

    Add all block types, random picker

--- src/tetroid.rs
+++ src/tetroid.rs
@@ -3,6 +3,9 @@
 use bevy::{prelude::*, utils::tracing::subscriber::DefaultGuard};
 use bevy_rapier2d::prelude::*;
 
+use rand::seq::SliceRandom;
+use rand::Rng;
+
 pub mod components;
 
 use crate::event_demo::{Tetroid, FRICTION, GROUND_Y};
@@ -96,41 +99,123 @@ impl TetroidColliderBundle {
     }
 }
 
-pub fn spawn_lblock(mut commands: Commands) {
-    //let square = Collider::cuboid(BRICK_DIM / 2.0, BRICK_DIM / 2.0);
-    let square = Collider::convex_hull(&[
+#[derive(Debug, Clone, Copy)]
+pub enum TetrominoType {
+    // symmetric
+    I,
+    O,
+    T,
+    // asymmetric
+    J, // reflection of L
+    L,
+    S,
+    Z, // reflection of S
+}
+
+impl TetrominoType {
+    /// Pick a random `Tetromino` variant.
+    pub fn random() -> Option<Self> {
+        let mut rng = rand::thread_rng();
+        Self::VARIANTS.choose(&mut rng).copied().map(|t| {
+            dbg!(t);
+            t
+        })
+    }
+
+    /// Pick a random variant and convert it into a `Vec` of relative position
+    /// and collider pairs.
+    pub fn random_colliders() -> Option<Vec<(Vec2, Collider)>> {
+        Self::random().map(Self::to_colliders)
+    }
+
+    pub fn to_colliders(self) -> Vec<(Vec2, Collider)> {
+        match self {
+            Self::I => Self::with_squares(&Self::IBLOCK),
+            Self::O => Self::with_squares(&Self::OBLOCK),
+            Self::L => Self::with_squares(&Self::LBLOCK),
+            Self::J => Self::with_squares(&Self::reflect(&Self::LBLOCK)),
+            Self::Z => Self::with_squares(&Self::ZBLOCK),
+            Self::S => Self::with_squares(&Self::reflect(&Self::ZBLOCK)),
+            Self::T => Self::with_squares(&Self::TBLOCK),
+        }
+    }
+
+    pub fn square() -> Collider {
+        Collider::convex_hull(&Self::SQUARE).unwrap()
+    }
+
+    fn with_squares(positions: &[Vec2]) -> Vec<(Vec2, Collider)> {
+        positions
+            .iter()
+            .copied()
+            .map(|p| (p, Self::square()))
+            .collect()
+    }
+
+    /// Reflect a slice of relative positions about the x-axis.
+    fn reflect(positions: &[Vec2]) -> Vec<Vec2> {
+        positions
+            .iter()
+            .copied()
+            .map(|Vec2 { x, y }| Vec2::new(x * -1.0, y))
+            .collect()
+    }
+
+    const VARIANTS: [Self; 4] = [Self::I, Self::O, Self::L, Self::Z];
+
+    pub const SQUARE: [Vec2; 4] = [
         Vec2::new(0.0, 0.0),
         Vec2::new(0.0, BRICK_DIM),
         Vec2::new(BRICK_DIM, 0.0),
         Vec2::new(BRICK_DIM, BRICK_DIM),
-    ])
-    .unwrap();
-
-    let lblock_compound = Collider::compound(vec![
-        // lblock has four squares
-        // | 0 |
-        // | 1 |
-        // | 2 | 3 |
-        // NB. coordinates are in local space, squares have no rotation
-        // NOTE: These must be ordered so that each element is contiguous with
-        // the elements before and after it.
-        (Vec2::new(0.0, 0.0), 0.0, square.clone()),
-        (Vec2::new(0.0, -BRICK_DIM), 0.0, square.clone()),
-        (Vec2::new(0.0, -2.0 * BRICK_DIM), 0.0, square.clone()),
-        (Vec2::new(BRICK_DIM, -2.0 * BRICK_DIM), 0.0, square.clone()),
-    ]);
-
-    let lblock_components = [
-        // lblock has four squares
-        // | 0 |
-        // | 1 |
-        // | 2 | 3 |
-        // NB. coordinates are in local space, squares have no rotation
-        (Vec2::new(0.0, 0.0), square.clone()),
-        (Vec2::new(0.0, -BRICK_DIM), square.clone()),
-        (Vec2::new(0.0, -2.0 * BRICK_DIM), square.clone()),
-        (Vec2::new(BRICK_DIM, -2.0 * BRICK_DIM), square.clone()),
     ];
+
+    const IBLOCK: [Vec2; 4] = [
+        Vec2::new(0.0, 0.0),
+        Vec2::new(0.0, -BRICK_DIM),
+        Vec2::new(0.0, -2.0 * BRICK_DIM),
+        Vec2::new(0.0, -3.0 * BRICK_DIM),
+    ];
+
+    const OBLOCK: [Vec2; 4] = [
+        Vec2::new(0.0, 0.0),
+        Vec2::new(-BRICK_DIM, -BRICK_DIM),
+        Vec2::new(-BRICK_DIM, 0.0),
+        Vec2::new(0.0, -BRICK_DIM),
+    ];
+
+    const ZBLOCK: [Vec2; 4] = [
+        Vec2::new(0.0, 0.0),
+        Vec2::new(0.0, -BRICK_DIM),
+        Vec2::new(-BRICK_DIM, -BRICK_DIM),
+        Vec2::new(-BRICK_DIM, 2.0 * -BRICK_DIM),
+    ];
+
+    const TBLOCK: [Vec2; 4] = [
+        Vec2::new(0.0, 0.0),
+        Vec2::new(-BRICK_DIM, 0.0),
+        Vec2::new(-BRICK_DIM * 2.0, 0.0),
+        Vec2::new(-BRICK_DIM, -BRICK_DIM),
+    ];
+
+    const LBLOCK: [Vec2; 4] = [
+        // lblock has four squares
+        // | 0 |
+        // | 1 |
+        // | 2 | 3 |
+        // NB. coordinates are in local space, squares have no rotation
+        Vec2::new(0.0, 0.0),
+        Vec2::new(0.0, -BRICK_DIM),
+        Vec2::new(0.0, -2.0 * BRICK_DIM),
+        Vec2::new(BRICK_DIM, -2.0 * BRICK_DIM),
+    ];
+}
+
+pub fn spawn_lblock(mut commands: Commands) {
+    //let square = Collider::cuboid(BRICK_DIM / 2.0, BRICK_DIM / 2.0);
+    let square = TetrominoType::square();
+
+    let tetromino = TetrominoType::random_colliders().unwrap();
 
     // active tetroid gets lower gravity
     let mut tetromino_bundle = TetrominoBundle {
@@ -151,66 +236,19 @@ pub fn spawn_lblock(mut commands: Commands) {
         .spawn(tetromino_bundle)
         .insert(ActiveTetroid)
         .with_children(|children| {
-            lblock_components.clone().into_iter().for_each(
-                |(Vec2 { x, y }, collider)| {
-                    let collider_bundle = TetroidColliderBundle {
-                        collider,
-                        ..Default::default()
-                    }
-                    .with_friction(FRICTION)
-                    .with_starting_position(x, y);
-                    children
-                        .spawn(collider_bundle)
-                        .insert(ActiveTetroidCollider);
-                },
-            )
+            tetromino.into_iter().for_each(|(Vec2 { x, y }, collider)| {
+                let collider_bundle = TetroidColliderBundle {
+                    collider,
+                    ..Default::default()
+                }
+                .with_friction(FRICTION)
+                .with_starting_position(x, y);
+                children
+                    .spawn(collider_bundle)
+                    .insert(ActiveTetroidCollider);
+            })
         })
         .id();
 
-    //let id = commands
-    //    .spawn(RigidBody::Dynamic)
-    //    .insert(Sleeping::default())
-    //    .with_children(|children| {
-    //        lblock_components.into_iter().for_each(
-    //            |(Vec2 { x, y }, shape)| {
-    //                children
-    //                    .spawn(shape)
-    //                    .insert(ActiveTetroidCollider)
-    //                    .insert(Tetroid)
-    //                    .insert(TransformBundle::from(Transform::from_xyz(
-    //                        x, y, 0.0,
-    //                    )))
-    //                    .insert(ActiveEvents::COLLISION_EVENTS)
-    //                    .insert(Friction {
-    //                        coefficient: 0.0,
-    //                        combine_rule: CoefficientCombineRule::Min,
-    //                    })
-    //                    .insert(Restitution {
-    //                        coefficient: 0.0,
-    //                        combine_rule: CoefficientCombineRule::Min,
-    //                    });
-    //            },
-    //        );
-    //    })
-    //    .insert(TransformBundle::from(Transform::from_xyz(
-    //        -BRICK_DIM,
-    //        GROUND_Y + BRICK_DIM * 21.0,
-    //        0.0,
-    //    )))
-    //    .insert(ExternalImpulse {
-    //        impulse: Vec2::new(0.0, 0.0),
-    //        torque_impulse: 0.0,
-    //    })
-    //    //.insert(ActiveEvents::COLLISION_EVENTS)
-    //    .insert(Ccd::enabled()) // enable continous collision detection
-    //    .insert(ActiveTetroid)
-    //    .insert(Tetroid)
-    //    .insert(Velocity {
-    //        linvel: Vec2::new(0.0, -120.0),
-    //        angvel: 0.0,
-    //    })
-    //    .insert(GravityScale(0.02))
-    //    .id();
-    //commands.entity(id2).log_components();
     info!("Spawned new lblock: {:?}", id2);
 }
